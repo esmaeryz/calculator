3ab216c8c3759aab476bfb844f6cfba7cb2b5cae
Esmanur Eryılmaz <77831768+esmaeryz@users.noreply.github.com>
2022-02-13 23:43:30 +0300

    Final correction about the buttons design and colors etc.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Calculator from './component/Calculator';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Calculator>
+      
+    </Calculator>
   );
 }
 
